5c926896bcc0d1d3f958736758899ce316382ac5
Greg Mulcair <info@awakensolutions.com>
2026-04-30 11:52:38 -0400

    Release v0.1.0

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_coursecalendar';
-$plugin->version   = 2026042406;
+$plugin->version   = 2026043000;
 $plugin->requires  = 2023042400;
 $plugin->maturity  = MATURITY_ALPHA;
 $plugin->release   = '0.1.0';
